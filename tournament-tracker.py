e713b6376e73ec343c50f3885050002e86c9c582
“Ahad <ahussain@dev-10.com>
2022-04-07 15:37:31 -0400

    Added error exceptions to Startup() and SignUp() Functions

--- tournament-tracker.py
+++ tournament-tracker.py
@@ -1,6 +1,6 @@
 def Validate_Input(message, input_type):
     valid_value = False
-    if input_type == 'num':
+    if input_type == 'number':
         while valid_value == False:
             try:
                 user_input = int(input(message))
@@ -8,7 +8,7 @@ def Validate_Input(message, input_type):
                 return user_input
             except ValueError:
                 print()
-                print("Invalid input.\n")
+                print("Error: Invalid input.\n")
                 continue
     
 
@@ -17,13 +17,13 @@ def StartUp():
     print("Welcome to Tournaments R Us\n============================")
     message = "Enter number of participants: "
     global num_participants
+    global participant_dict
 
-    input_type = 'num'
-    num_participants = Validate_Input(message, input_type)
-    while num_participants == 1:
+    num_participants = Validate_Input(message, 'number')
+    while num_participants <= 1:
         print()
-        print('The number of participants must be more than 1!\n')
-        num_participants = Validate_Input(message, input_type)
+        print('Error: The number of participants must be more than 1!\n')
+        num_participants = Validate_Input(message, 'number')
 
     print(f"There are {num_participants} participant slots ready for sign-ups.\n\n")
 
@@ -32,13 +32,24 @@ def StartUp():
         participant_dict.update({i:None}) 
 
 
+def SignUp(run_number = 1):
+    global participant_name
+    if run_number == 1:
+        print("Participant Sign Up\n====================")
+        participant_name = input("Participant Name: ")
+    slot_message = f"Desired starting slot #[1-{num_participants}]: "
+    starting_slot = Validate_Input(slot_message, 'number')
 
-def SignUp():
-
-    print("Participant Sign Up\n====================")
-    participant_name = input("Participant Name: ")
-    starting_slot = input(f"Desired starting slot #[1-{num_participants}]: ")
-
+    while starting_slot < 1 or starting_slot > num_participants:
+        print()
+        print('Error: Out of range!\n')
+        starting_slot = Validate_Input(slot_message, 'number')
+    print()
+    if participant_dict[starting_slot] != None:
+        print(f"Error:\nSlot #{starting_slot} is filled. Please try again.\n")
+        SignUp(2)
+    else:
+        participant_dict[starting_slot] = participant_name
 
 StartUp()
 SignUp()
