15926b303a18f9787badf033ce9f2850178485a5
“Ahad <ahussain@dev-10.com>
2022-04-07 14:46:01 -0400

    Added Validate_Input() function

--- tournament-tracker.py
+++ tournament-tracker.py
@@ -1,16 +1,31 @@
-def StartUp():
-    print("Welcome to Tournaments R Us\n============================")
+def Validate_Input(message, input_type):
     valid_value = False
-    while valid_value == False:
-        try:
-            global num_participants
-            num_participants = int(input("Enter number of participants: "))
-            valid_value = True
-        except ValueError:
-            print("Invalid input.")
-            continue
+    if input_type == 'num':
+        while valid_value == False:
+            try:
+                user_input = int(input(message))
+                valid_value = True
+                return user_input
+            except ValueError:
+                print()
+                print("Invalid input.\n")
+                continue
+    
+
+def StartUp():
     print()
-    print(f"There are {num_participants} participant slots ready for sign-ups.")
+    print("Welcome to Tournaments R Us\n============================")
+    message = "Enter number of participants: "
+    global num_participants
+
+    input_type = 'num'
+    num_participants = Validate_Input(message, input_type)
+    while num_participants == 1:
+        print()
+        print('The number of participants must be more than 1!\n')
+        num_participants = Validate_Input(message, input_type)
+
+    print(f"There are {num_participants} participant slots ready for sign-ups.\n\n")
 
     participant_dict = {}
     for i in range(1,num_participants+1):
